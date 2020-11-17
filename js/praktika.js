6a4a4562e7571b2d7949264f787e651cc94899bb
Awday <example@awday.com>
2020-11-17 22:39:39 +0100

    pervaja praktika

---
+++ js/praktika.js
@@ -0,0 +1,18 @@
+const numberOfFilms = prompt("How many moves did you watch?", "");
+
+const obj = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+const lastMovie = prompt("What was the last movie you watched?", "");
+const lastMovieRate = prompt("How do you rate the last movie?", "");
+const lastMovie1 = prompt("What was the last movie you watched?", "");
+      lasMovieRate1 = prompt("How do you rate the last movie?", "");
+
+obj.movies[lastMovie] = lastMovieRate;
+obj.movies[lastMovie1] = lasMovieRate1;
+
+console.log(obj);
