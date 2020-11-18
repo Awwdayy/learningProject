946ec24a053534011bbdd90bb1e2f8cc37a2b5c3
Awday <example@awday.com>
2020-11-18 18:34:44 +0100

    praktika nr2. cikli i usloviya(Ispravleno)

--- js/praktika.js
+++ js/praktika.js
@@ -1,17 +1,17 @@
 "use strict";
 
 let numberOfFilms = prompt("How many movies did you watch?", "");
-while (numberOfFilms == null){
-    numberOfFilms = prompt("How many movies did you watch?", "");
-    if (numberOfFilms != null){
-        while(numberOfFilms.length < 1 || numberOfFilms.length > 50){
-            numberOfFilms = prompt("How many movies did you watch?", "");
-        }
-    }
-}
+// while (numberOfFilms == null){
+//     numberOfFilms = prompt("How many movies did you watch?", "");
+//     if (numberOfFilms != null){
+//         while(numberOfFilms.length < 1 || numberOfFilms.length > 50){
+//             numberOfFilms = prompt("How many movies did you watch?", "");
+//         }
+//     }
+// }
 
 const obj = {
-    count: +numberOfFilms,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
@@ -20,7 +20,7 @@ const obj = {
 
 if (obj.count < 10 && obj.count >= 0){
     alert("Просмотрено мало фильмов!");
-} else if (obj.count > 10 && obj.count < 30){
+} else if (obj.count >= 10 && obj.count <= 30){
     alert("Вы классический зритель!");
 } else if (obj.count > 30){
     alert("Вы киноман!");
@@ -30,23 +30,27 @@ if (obj.count < 10 && obj.count >= 0){
 
 for (let i = 0; i < 2; i++){
     let a = prompt("What was the last movie you watched?", "");
-    while (a == null){
-        a = prompt("What was the last movie you watched?", "");
-        if(a != null){
-            while(a.length < 1 || a.length > 50){
-                a = prompt("What was the last movie you watched?", "");
-            }
-        }
-    }
+    // while (a == null){
+    //     a = prompt("What was the last movie you watched?", "");
+    //     if(a != null){
+    //         while(a.length < 1 || a.length > 50){
+    //             a = prompt("What was the last movie you watched?", "");
+    //         }
+    //     }
+    // }
     let b = prompt("How do you rate the last movie?", "");
-    while (b == null){
-        b = prompt("How do you rate the last movie?", "");
-        if(b != null){
-            while((b.length > 50) || (b.length < 1)){
-                b = prompt("How do you rate the last movie?", "");
-            }
-        }
-    } 
-    obj.movies[a] = b;
+    // while (b == null){
+    //     b = prompt("How do you rate the last movie?", "");
+    //     if(b != null){
+    //         while((b.length > 50) || (b.length < 1)){
+    //             b = prompt("How do you rate the last movie?", "");
+    //         }
+    //     }
+    // } 
+    if (a != null && b != null && a.length != "" && b.length != "" && a.length < 50 && b.length < 50){
+        obj.movies[a] = b;
+    } else {
+        i--;
+    }
 }
 console.log(obj);
