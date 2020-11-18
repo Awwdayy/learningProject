c027e8a30d1674aa93b97f1a6b0b28be009e3e71
Awday <example@awday.com>
2020-11-18 15:52:00 +0100

    praktika cikli i usloviya

--- js/praktika.js
+++ js/praktika.js
@@ -1,18 +1,52 @@
-const numberOfFilms = prompt("How many moves did you watch?", "");
+"use strict";
+
+let numberOfFilms = prompt("How many movies did you watch?", "");
+while (numberOfFilms == null){
+    numberOfFilms = prompt("How many movies did you watch?", "");
+    if (numberOfFilms != null){
+        while(numberOfFilms.length < 1 || numberOfFilms.length > 50){
+            numberOfFilms = prompt("How many movies did you watch?", "");
+        }
+    }
+}
 
 const obj = {
-    count: numberOfFilms,
+    count: +numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
+};
+
+if (obj.count < 10 && obj.count >= 0){
+    alert("Просмотрено мало фильмов!");
+} else if (obj.count > 10 && obj.count < 30){
+    alert("Вы классический зритель!");
+} else if (obj.count > 30){
+    alert("Вы киноман!");
+} else {
+    alert("Произошла ошибка!");
 }
-const lastMovie = prompt("What was the last movie you watched?", "");
-      lastMovieRate = prompt("How do you rate the last movie?", "");
-      lastMovie1 = prompt("What was the last movie you watched?", "");
-      lasMovieRate1 = prompt("How do you rate the last movie?", "");
 
-obj.movies[lastMovie] = lastMovieRate;
-obj.movies[lastMovie1] = lasMovieRate1;
-
-console.log(obj.movies);
+for (let i = 0; i < 2; i++){
+    let a = prompt("What was the last movie you watched?", "");
+    while (a == null){
+        a = prompt("What was the last movie you watched?", "");
+        if(a != null){
+            while(a.length < 1 || a.length > 50){
+                a = prompt("What was the last movie you watched?", "");
+            }
+        }
+    }
+    let b = prompt("How do you rate the last movie?", "");
+    while (b == null){
+        b = prompt("How do you rate the last movie?", "");
+        if(b != null){
+            while((b.length > 50) || (b.length < 1)){
+                b = prompt("How do you rate the last movie?", "");
+            }
+        }
+    } 
+    obj.movies[a] = b;
+}
+console.log(obj);
